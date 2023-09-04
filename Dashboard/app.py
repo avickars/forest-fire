ff861169734a21e265da8422dbfdeaa1f4b48884
Aidan Vickars <aidanvickars@gmail.com>
2023-09-04 16:18:50 -0700

    again

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -2,4 +2,4 @@ import dash
 import dash_bootstrap_components as dbc
 app_dash = dash
 app = app_dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
-server = app.server
+# server = app.server
